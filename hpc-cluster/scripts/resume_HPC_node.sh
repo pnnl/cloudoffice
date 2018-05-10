6acddf34d85bd4e59d04d6da9e182b66fabda171
Jensen, Jason E <jason.jensen@pnnl.gov>
2018-05-10 13:56:18 -0700

    Change to use service principal

--- hpc-cluster/scripts/resume_HPC_node.sh
+++ hpc-cluster/scripts/resume_HPC_node.sh
@@ -8,7 +8,7 @@ echo "`date` Resume invoked $0 $*" >>/var/log/power_save.log
 resourceGroup="hpctest2"
 
 az cloud set --name AzureUSGovernment
-az login
+az login --service-principal --username 52404f5f-5455-42c6-bcde-446a4dee6304 --tenant pnnlcloudoffice.onmicrosoft.com --password /home/hpc/hpctest.pem
 az account set --subscription d9eabcc0-f041-4271-b900-25ec822ae637
 #az vm create --name $vmName --resource-group $resourceGroup --private-ip-address $staticIP --ssh-key-value "$(< ~/.ssh/id_rsa.pub)" --vnet-name $vnetName --image $vmImage --admin-username $adminUsername --size $vmSize --public-ip-address "" --nsg $nsgName --no-wait
 

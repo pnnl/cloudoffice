9f6f62aba18a9c8df5513f9329c5ef3e7a8a8ceb
Jensen, Jason E <jason.jensen@pnnl.gov>
2018-05-10 11:12:54 -0700

    Scripts for slurm suspend/resume and installing az cli

---
+++ hpc-cluster/scripts/suspend_HPC_node.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#parameters needed
+#hostlist in Slurm hostlist expression format
+
+
+echo "`date` Suspend invoked $0 $*" >>/var/log/power_save.log
+
+resourceGroup="hpctest2"
+
+az cloud set --name AzureUSGovernment
+az login
+az account set --subscription d9eabcc0-f041-4271-b900-25ec822ae637
+#az vm create --name $vmName --resource-group $resourceGroup --private-ip-address $staticIP --ssh-key-value "$(< ~/.ssh/id_rsa.pub)" --vnet-name $vnetName --image $vmImage --admin-username $adminUsername --size $vmSize --public-ip-address "" --nsg $nsgName --no-wait
+
+hosts=`scontrol show hostnames $1`
+for host in $hosts
+do
+  az vm stop --resource-group $resourceGroup --no-wait --name $host
+done

eaae183eea851bfab76edbdcdc451a22ac27dd78
Jason <jason.jensen@pnnl.gov>
2019-03-18 08:48:11 -0700

    Modified for commercial cloud environment

--- hpc-cluster/scripts/suspend_HPC_node.sh
+++ hpc-cluster/scripts/suspend_HPC_node.sh
@@ -5,11 +5,11 @@
 
 echo "`date` Suspend invoked $0 $*" >>/var/log/power_save.log
 
-resourceGroup="hpctest2"
+resourceGroup="hpctesthc"
 
-az cloud set --name AzureUSGovernment
-az login --service-principal --username 52404f5f-5455-42c6-bcde-446a4dee6304 --tenant pnnlcloudoffice.onmicrosoft.com --password /share/nfs/apps/scripts/hpctest.pem
-az account set --subscription d9eabcc0-f041-4271-b900-25ec822ae637
+az cloud set --name AzureCloud
+az login --service-principal --username 58a7c641-ea6f-4c8c-841a-ad48b421a086 --tenant d6faa5f9-0ae2-4033-8c01-30048a38deeb --password /share/nfs/apps/scripts/hpctest.pem
+az account set --subscription a96c7ce0-7f4a-4b6f-a7c4-8c11818e5799
 #az vm create --name $vmName --resource-group $resourceGroup --private-ip-address $staticIP --ssh-key-value "$(< ~/.ssh/id_rsa.pub)" --vnet-name $vnetName --image $vmImage --admin-username $adminUsername --size $vmSize --public-ip-address "" --nsg $nsgName --no-wait
 
 hosts=`scontrol show hostnames $1`
